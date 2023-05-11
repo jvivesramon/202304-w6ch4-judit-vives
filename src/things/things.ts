3d28af5892bd838ee944b6f377ea965fc45ff096
Judit Vives <jvivesramon@gmail.com>
2023-05-11 23:41:41 +0200

    Add things data an types files

---
+++ src/things/things.ts
@@ -0,0 +1,22 @@
+import { type ThingDataStructure } from "./types";
+
+const things: ThingDataStructure[] = [
+  {
+    id: "1",
+    name: "The random is not random",
+  },
+  {
+    id: "2",
+    name: "Coder Cat will rule us all",
+  },
+  {
+    id: "3",
+    name: "Mario can't whisper",
+  },
+  {
+    id: "4",
+    name: "Constantin is the undercover boss",
+  },
+];
+
+export default things;
